0b1f17765f094f8e3aba56462aa66162088a6a83
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-08-19 16:03:05 +0000

    [add] 商品新規登録フォーム（その他アクション/ビューの作成）

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,2 +1,44 @@
 class Admin::ProductsController < ApplicationController
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_product_path(@product.id)
+    else
+      render :new
+    end
+  end
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to admin_product_path(@product.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:genre_id, :name, :price, :detail, :is_available, :image)
+  end
+
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -14,12 +14,12 @@ class Product < ApplicationRecord
   presence: true
 
   # 画像呼び出しメソッド
-　def get_image(width, height)
+  def get_image(width, height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     image.variant(resize_to_limit: [width, height]).processed
+  end
 
 end
-
