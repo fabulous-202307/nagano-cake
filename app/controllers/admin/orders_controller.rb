d4013884550b7baa01c1134184800104d147b0a5
EC2 Default User <ec2-user@ip-172-31-40-61.ap-northeast-1.compute.internal>
2023-08-20 02:07:27 +0000

    [update] 注文ステータス・注文履歴の更新

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,15 +2,21 @@ class Admin::OrdersController < ApplicationController
   before_action :authenticate_admin!
   before_action :set_order
 
-  def show; end
-
+  
+  # 注文のステータスを更新するアクション
   def update
-    @order.update(order_params)
+    @order = Order.find(params[:id])
+    @order_items = @order.order_items
+    if @order.update(order_params)
+      # ①注文ステータスが「入金確認」とき、製作ステータスを全て「製作待ち」に更新する
+      @order_items.update_all(making_status: 1) if @order.status == "payment_confirmation"
+    end
     redirect_to admin_order_path(@order)
   end
 
   private
-
+  
+   # ストロングパラメーターの定義
   def set_order
     @order = Order.find(params[:id])
   end
--- config/application.rb
+++ config/application.rb
@@ -12,11 +12,14 @@ module NaganoCake
     config.load_defaults 6.1
 
     # Configuration for the application, engines, and railties goes here.
-    config.i18n.default_locale = :ja
+
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.i18n.default_locale = :ja
+
   end
 end
