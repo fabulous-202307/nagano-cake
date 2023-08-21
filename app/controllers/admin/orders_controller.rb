f38e0a3d5ddcdff23fa6fb45f1239b9ae9447740
EC2 Default User <ec2-user@ip-172-31-40-61.ap-northeast-1.compute.internal>
2023-08-21 11:31:20 +0000

    [fix]トップページ修正

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,23 +1,2 @@
 class Admin::OrdersController < ApplicationController
-  before_action :authenticate_admin!
-
-  
-  # 注文のステータスを更新するアクション
-  def update
-    @order = Order.find(params[:id])
-    @order_items = @order.order_items
-    if @order.update(order_params)
-      # ①注文ステータスが「入金確認」とき、製作ステータスを全て「製作待ち」に更新する
-      @order_items.update_all(making_status: 1) if @order.status == "payment_confirmation"
-    end
-    redirect_to admin_order_path(@order)
-  end
-
-  private
-  
-   # ストロングパラメーターの定義
-
-  def order_params
-    params.require(:order).permit(:status)
-  end
 end
