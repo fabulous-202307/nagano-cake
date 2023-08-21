9b9f2532eb9eb097ad173ee9f94ec372bb6aeaee
EC2 Default User <ec2-user@ip-172-31-3-23.ap-northeast-1.compute.internal>
2023-08-21 09:41:12 +0000

    [fix]bootstrap

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -4,5 +4,11 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
+  end
+  
+  private
+  def list_params
+    params.require(:list).permit(:last_name, :first_name, :last_name_kane, :first_name_kana, :email, :post_code, :address, :phone_number,)
   end
 end
