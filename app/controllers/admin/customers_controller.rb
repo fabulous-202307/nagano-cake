644c1aca597031916d9583dfbf1f853d923d158e
EC2 Default User <ec2-user@ip-172-31-40-61.ap-northeast-1.compute.internal>
2023-08-21 02:44:01 +0000

    [add]顧客詳細画面・顧客編集画面追加

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -3,17 +3,28 @@ class Admin::CustomersController < ApplicationController
   def index
     @customers = Customer.all
   end
-  
-  def new
-    
-  end
-  
+
   def show
+    @customers = Costomer.find(params[:id])
   end
-  
+
   def edit
+    @customers = Costomer.find(params[:id])
   end
-  
+
   def update
+    @customers = Costomer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer)
+    else
+      render edit_customer_path
   end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :address, :phone_number, :email, :is_valid_status)
+  end
+
+
 end
