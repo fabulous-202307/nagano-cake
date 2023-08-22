7e89f8be4b70d52eb3984fb5e64baf23d5abd8cd
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-08-22 18:49:33 +0000

    [update] active_storage/resize_to_fitに変更

--- app/models/product.rb
+++ app/models/product.rb
@@ -19,9 +19,9 @@ class Product < ApplicationRecord
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image.variant(resize_to_limit: [width, height]).processed
+    image.variant(resize_to_fill: [width, height]).processed
   end
-  
+
   def with_tax_price
     (price * 1.1).floor
   end
