6f24d0eea6a6a2f78692c4edb1ff13fe0be1c170
EC2 Default User <ec2-user@ip-172-31-3-23.ap-northeast-1.compute.internal>
2023-08-22 12:07:15 +0000

    [add]ヘッダーにユーザー名表示

--- config/routes.rb
+++ config/routes.rb
@@ -9,15 +9,20 @@ Rails.application.routes.draw do
   devise_for :admin,skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
-  
+
   namespace :admin do
-    resources :products, only: [:new, :create, :index, :show, :edit, :update, ]
+    resources :products, only: [:new, :create, :index, :show, :edit, :update]
     resources :genres, only: [:index, :create, :edit, :update]
-  
   end
 
 
   scope module: :public do
+    get "customer/mypage" => "customers#mypage", as:"mypage"
+    get "customer/infomation/edit/:full_name" => "customers#edit", as:"infomation"
+    get "customer/infomatio/:full_namen" => "customers#update"
+    get "customer/confirm_withdraw" => "customuers#confirm_withdraw", as:"confirm_withdraw"
+    get "customers/withdraw/:full_name" => "customers#withdraw", as:"withdraw"
+    patch  '/customers/withdraw' => 'customers#withdraw'
     root to: 'homes#top'
   end
 
