adfef3dd4b505d1d856b68f2be81854ae138679c
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-08-22 06:35:23 +0000

    [add] customer/products（コントローラ、アクション、ルーティング）

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,2 +1,18 @@
 class Public::ProductsController < ApplicationController
+  
+  def index
+    @products = Product.all
+    @genres = Genre.all
+  end
+  
+  def show
+    @product = Product.find(params[:id])
+  end
+  
+  private
+
+  def product_params
+    params.require(:product).permit(:genre_id, :name, :price, :detail, :is_available, :image)
+  end
+  
 end
