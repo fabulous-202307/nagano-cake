2666f672d65b86d3d64c79793a3b5b92b1c8c93c
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-08-22 15:48:11 +0000

    [fix] conflict解消/products_controller

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,25 +1,20 @@
 class Public::ProductsController < ApplicationController
-  
+
   def index
-<<<<<<< HEAD
     @products = Product.page(params[:page])
     @genres = Genre.all
     @count_products = Product.count
-    
-=======
-    @products = Product.all
-    @genres = Genre.all
->>>>>>> adfef3dd4b505d1d856b68f2be81854ae138679c
+
   end
-  
+
   def show
     @product = Product.find(params[:id])
   end
-  
+
   private
 
   def product_params
     params.require(:product).permit(:genre_id, :name, :price, :detail, :is_available, :image)
   end
-  
+
 end
