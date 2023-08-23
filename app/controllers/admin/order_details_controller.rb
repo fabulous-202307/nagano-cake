4d6ccaa46c98ba16198b7fc6fce63138f1000b4c
EC2 Default User <ec2-user@ip-172-31-40-61.ap-northeast-1.compute.internal>
2023-08-23 09:30:43 +0000

    [update]order_detail更新中

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -6,11 +6,13 @@ class Admin::OrderDetailsController < ApplicationController
     @order_details = @order.order_details
     @order_detail.update(order_detail_params)
 
+    #もし「製作中」の注文詳細が1つ以上ある場合、注文のステータスを "製作中" に更新する
     if @order_details.where(production_status:"製作中").count >= 1
       @order.status = "製作中"
       @order.save
     end
     
+    #もしすべての注文詳細が「製作完了」の場合、注文のステータスを "発送準備中" に更新する
     if @order.order_details.count == @order_details.where(production_status: "製作完了").count
       @order.status = "発送準備中"
       @order.save
--- app/models/product.rb
+++ app/models/product.rb
@@ -13,12 +13,4 @@ class Product < ApplicationRecord
             :detail,
   presence: true
 
-  # 画像呼び出しメソッド
-  def get_image(width, height)
-    unless image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-    end
-    image.variant(resize_to_limit: [width, height]).processed
-  end
 end
