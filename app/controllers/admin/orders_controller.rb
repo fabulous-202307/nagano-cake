8fb5708087bc9595afca4b4d614c33a0bcac890b
EC2 Default User <ec2-user@ip-172-31-40-61.ap-northeast-1.compute.internal>
2023-08-24 07:34:52 +0000

    [fix]orderコントローラー修正

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,33 +1,35 @@
 class Admin::OrdersController < ApplicationController
 
-  def index
-    @orders = Order.all.page(params[:page]).per(10).order('created_at DESC')
-  end
+ def index
+  @orders = Order.page(params[:page])
+ end
 
-  def show
-    @order = Order.find(params[:id])
-    @order_details = @order.order_details
-    @full_name = @order.customer.last_name + "" + @order.customer.first_name
-  end
+ def show
+  @order = Order.find(params[:id])
+  @order_details = OrderDetail.where(order_id: @order.id)
+  @full_name = @order.customer.last_name + "" + @order.customer.first_name
+ end
 
+ def update
+   @order = Order.find(params[:id])
+   @order.update(order_params)
+   @order_details = @order.order_details
 
-  def update
-    @order = Order.find(params[:id])
-    @order.update(order_params)
-    @order_details = @order.order_details
+   #もし注文のステータスが「入金確認」の場合、以下の処理を行う
+   if @order.status == "入金確認"
+     @order_details.each do |order_detail|
+     order_detail.production_status = "製作待ち"
+     order_detail.save
+     end
+   end
+  redirect_to admin_order_path
+ end
 
-    if @order.status == "入金確認"
-      @order_details.each do |order_detail|
-        order_detail.production_status = "製作待ち"
-        order_detail.save
-      end
-    end
-    redirect_to admin_order_path
-  end
+ private
 
-  private
+ def order_params
+  params.require(:order).permit(:status)
+ end
 
-  def order_params
-    params.require(:order).permit(:status)
-  end
 end
+
