00932ab12052d018e59897962956bea779ad0860
EC2 Default User <ec2-user@ip-172-31-3-23.ap-northeast-1.compute.internal>
2023-08-26 08:00:13 +0000

    [fix]フラッシュメッセージ

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
-   before_action :customer_state, only: [:create, :customer_state]
+   before_action :customer_state, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -35,15 +35,15 @@ class Public::SessionsController < Devise::SessionsController
 
   protected
   def customer_state
-    @customer = Customer.find_by(email: params[:email])
-    return if !@customer
-    if @customer.valid_password?(params[:customer][:password]) && (@customer.is_valid == true)
-      redirect_to root_path
-    else
-      flash[:notice] = "退会済みです。再度ご登録をしてご利用ください。"
-      redirect_to new_customer_registration_path
-    end
+    @customer = Customer.find_by(email: params[:customer][:email])
+    if @customer
+      if @customer.valid_password?(params[:customer][:password]) && (@customer.is_valid == false)
+         flash[:notice] = "退会済みです。再度ご登録をしてご利用ください。"
+         redirect_to new_customer_registration_path
+      else
 
+      end
+    end
   end
 
 
