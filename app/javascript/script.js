051f90afef5b4341b409b6fc21056b3b131267b2
EC2 Default User <ec2-user@ip-172-31-40-61.ap-northeast-1.compute.internal>
2023-08-26 18:17:02 +0000

    [fit]javascript 修正

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
-  $(".slider").slick({
+  $('.slider').slick({
     infinite: true,
+    variableWidth: true,
+    centerMode: true,
+    speed: 300,
+    slidesToShow: 3,
     slidesToScroll: 1,
     dots: true,
-    centerMode: true,
-    variableWidth: true
   });
 });
