01ddd6b2d7afd25a1e557116d72cb11b32566000
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-08-27 12:10:07 +0000

    [update] layout色々

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -2,7 +2,7 @@ class Admin::GenresController < ApplicationController
   before_action :authenticate_admin!
   
   def index
-    @genres = Genre.all
+    @genres = Genre.page(params[:page]).per(6)
     @genre = Genre.new
   end
   
