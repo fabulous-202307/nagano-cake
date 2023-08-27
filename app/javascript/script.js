6c1f65bfb8ed829dae455da831c5db799a8c828e
EC2 Default User <ec2-user@ip-172-31-40-61.ap-northeast-1.compute.internal>
2023-08-27 06:36:40 +0000

    [update]top/aboutページ調整

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -7,5 +7,7 @@ $(document).ready(function() {
     slidesToShow: 3,
     slidesToScroll: 1,
     dots: true,
+    prevArrow: '<div class="slick-prev"></div>', // 前へボタンのHTMLコードを指定
+    nextArrow: '<div class="slick-next"></div>', // 次へボタンのHTMLコードを指定
   });
 });
