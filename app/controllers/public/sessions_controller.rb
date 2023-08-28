eebaf1875c7ca9e959ee90173b3ab8618d4edca1
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-08-28 04:39:03 +0000

    [Finish]お疲れ様でしたー！

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -38,7 +38,7 @@ class Public::SessionsController < Devise::SessionsController
     @customer = Customer.find_by(email: params[:customer][:email])
     if @customer
       if @customer.valid_password?(params[:customer][:password]) && (@customer.is_valid == false)
-         flash[:notice] = "退会済みです。再度ご登録をしてご利用ください。"
+         flash[:alert] = "退会済みです。再度ご登録をしてご利用ください。"
          redirect_to new_customer_registration_path
       else
 
